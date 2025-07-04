a30197644d24698617a747c88bbb08e153ad1280
Phinguyen <phinguyen2413@gmail.com>
2025-07-04 20:05:46 +0700

    deepseekv3free

---
+++ LLM_RAG_Backend/deepseek_v3.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+from llama_vision import call_api_llamavision
+from api_key import load_openrouter_apikey
+
+api_key = load_openrouter_apikey()
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=api_key,
+  timeout=10
+)
+
+def call_api_llm(prompt, entries=3):
+  for i in range(entries):
+    try:
+        completion = client.chat.completions.create(
+        model="deepseek/deepseek-chat-v3-0324:free",
+        messages=[
+            {
+            "role": "user",
+            "content": prompt
+            }
+        ]
+        )
+        return completion.choices[0].message.content
+    except:
+        call_api_llamavision(prompt)
+  return "⚠️ Server đang quá tải, vui lòng thử lại sau."
