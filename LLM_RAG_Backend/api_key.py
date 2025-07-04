f0d613559722d7ed6bdd1aa1c2925767ef78a469
Phinguyen <phinguyen2413@gmail.com>
2025-07-04 20:04:40 +0700

    update apikey

--- LLM_RAG_Backend/api_key.py
+++ LLM_RAG_Backend/api_key.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 import os
 
-def load_apikey():
+def load_together_apikey():
     load_dotenv(".env")  
     api_key = os.getenv("TOGETHER_API_KEY")
+    return api_key
+
+def load_openrouter_apikey():
+    load_dotenv(".env")  
+    api_key = os.getenv("OPENROUTER_API_KEY")
     return api_key
