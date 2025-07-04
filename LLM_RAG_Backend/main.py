8888504da8bbe223f44d3969e21001cf30194dcf
phinguyen03 <92086378+phinguyen03@users.noreply.github.com>
2025-07-04 20:29:02 +0700

    Update main.py

--- LLM_RAG_Backend/main.py
+++ LLM_RAG_Backend/main.py
@@ -40,5 +40,5 @@ async def ask(query: str = Form(...)):
           "answer": answer})
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="127.0.0.1", port=8001)
+   uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
