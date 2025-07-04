839088c5a006fcc91e9adb8859823bd5386f5d28
phinguyen03 <92086378+phinguyen03@users.noreply.github.com>
2025-07-04 17:45:23 +0700

    Update llama_vision.py

--- LLM_RAG_Backend/llama_vision.py
+++ LLM_RAG_Backend/llama_vision.py
@@ -5,14 +5,18 @@ api_key = apikey()
 
 client = Together(api_key=api_key)
 def call_api_llm(prompt):
-  response = client.chat.completions.create(
-      model="meta-llama/Llama-Vision-Free",             #https://api.together.ai/models
-      messages=[
-        {
-          "role": "user",
-          "content": prompt
-        }
-      ]
-  )
+  try:
+    response = client.chat.completions.create(
+        model="meta-llama/Llama-Vision-Free",             #https://api.together.ai/models
+        messages=[
+          {
+            "role": "user",
+            "content": prompt
+          }
+        ]
+    )
   #print(f"🤖 Trợ lý -> {response.choices[0].message.content}")
-  return response.choices[0].message.content
+    return response.choices[0].message.content
+  except Exception as e:
+    print("❌ Lỗi call_api_llm:", e, flush=True)
+    return "❌ Lỗi khi gọi model"
