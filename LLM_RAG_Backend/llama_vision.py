feeb99c510b232e32694ae719823b2c66110fa03
Phinguyen <phinguyen2413@gmail.com>
2025-07-04 20:05:15 +0700

    update llm

--- LLM_RAG_Backend/llama_vision.py
+++ LLM_RAG_Backend/llama_vision.py
@@ -1,10 +1,11 @@
 from together import Together
-from api_key import load_apikey
+from api_key import load_together_apikey
 
-api_key = load_apikey()
+api_key = load_together_apikey()
 
-client = Together(api_key=api_key)
-def call_api_llm(prompt):
+client = Together(api_key=api_key, timeout=10)
+
+def call_api_llamavision(prompt):
   try:
     response = client.chat.completions.create(
         model="meta-llama/Llama-Vision-Free",             #https://api.together.ai/models
@@ -19,4 +20,5 @@ def call_api_llm(prompt):
     return response.choices[0].message.content
   except Exception as e:
     print("❌ Lỗi call_api_llm:", e, flush=True)
+
 
