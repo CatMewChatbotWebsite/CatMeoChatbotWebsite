611c6cc1064f77f9d065c1a2e1a89685709bb25f
Phinguyen <phinguyen2413@gmail.com>
2025-07-08 12:40:02 +0700

    update class OÔP

--- LLM_RAG_Backend/vectorDB.py
+++ LLM_RAG_Backend/vectorDB.py
@@ -1,121 +1,116 @@
 from sentence_transformers import SentenceTransformer
 from pdf_extract import Pdf_loading, text_chunk_split
-import torch
-import chromadb
 from pprint import pprint
 from deepseek_v3 import call_api_llm
 from huggingface_hub import InferenceClient
-import os
-from api_key import load_huggingface_token
 from pinecone import Pinecone
-from api_key import load_pinecone_apikey
-
-Path = 'dataPDF.pdf'
-#Chromadb_Path = 'Chromadb'
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-os.environ["TRANSFORMERS_CACHE"] = "/tmp/empty_hf_cache"  # chống dùng cache cũ
-print(os.environ['TRANSFORMERS_CACHE'])
-
-chunk_text = []
-vectors = []
-index = []
-
-token = load_huggingface_token()
-
-api_key_pinecone = load_pinecone_apikey()
-pc = Pinecone(api_key=api_key_pinecone)
-index_name = "chatbot"
-namespace = "default"
-
-def get_model_embedding():
-    """
-        Local Embedding
-        return: list[embedding]
-    """
-    model_embeddings = SentenceTransformer(
-    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
-    device='cpu'
-)   
-    
-    return model_embeddings
-    
-def embedding_MiniLM_api(text):
-    """
-        API Embedding
-        return: list[embedding]
-    """
-    client = InferenceClient(
-        provider="hf-inference",
-        token=token,
-        model="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-    )
-
-    embedding = client.feature_extraction(text)
-    return embedding.tolist()
 
 
-def get_vectorstore(docs):
-    # convert docs_split to vectors:
-    for idx, doc in enumerate(docs):
-        chunk_text.append(doc.page_content)
-        index.append(str(idx))
-        vectors.append(embedding_MiniLM_api(doc.page_content))
-    
-    return vectors, index, chunk_text
 
-def get_index_pinecone(index_name):
-    if not pc.has_index(index_name):
-        pc.create_index(
-            name=index_name,
-            dimension=384,
-            metric='cosine',
-            spec={
-                "serverless": {
-                    "cloud": "aws",
-                    "region": "us-east-1"
-                }
-            }
-            
+
+class RAG:
+    def __init__(self, Path, index_name, namespace, HF_token, api_key_pinecone, device):
+        self.Path_PDF = Path
+        self.index_name = index_name
+        self.namespace = namespace
+        self.provider_HF = "hf-inference"
+        self.HF_token = HF_token
+        self.pc = Pinecone(api_key=api_key_pinecone)
+        self.model_embedding = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+        self.device = device
+        self.chunk_text = []
+        self.vectors = []
+        self.index = []
+
+    def get_model_embedding_local(self):
+        """
+            Local Embedding
+            return: list[embedding]
+        """
+        model_embeddings = SentenceTransformer(
+        self.model_embedding,
+        self.device
+    )   
+        
+        return model_embeddings
+        
+    def embedding_MiniLM_api(self, text):
+        """
+            API Embedding
+            return: list[embedding]
+        """
+        client = InferenceClient(
+            provider=self.provider_HF,
+            token=self.HF_token,
+            model=self.model_embedding
         )
-    return pc.Index(index_name)
 
-def get_vectorstore_pinecone(index_name, ids, embedding, chunk_text):
-    index = get_index_pinecone(index_name)
-    data = [ {
-            "id": str(ids),
-            "values": embedding[0],
-            "metadata": {"text": chunk_text}
-        }]
-    index.upsert(
-        namespace=namespace,
-        vectors=data
-    )
-    return index
+        embedding = client.feature_extraction(text)
+        return embedding.tolist()
 
 
-def prompting(docs, user_input):
-    context = "\n\n".join([doc for doc in docs])
-    prompt = f"""
-        Dựa vào thông tin sau, hãy trả lời ngắn gọn và tự nhiên:
-        {context}
-        Câu hỏi: {user_input}
-        Trả lời: 
-    """
-    return prompt
+    def get_vectorstore(self, docs):
+        # convert docs_split to vectors:
+        for idx, doc in enumerate(docs):
+            self.chunk_text.append(doc.page_content)
+            self.index.append(str(idx))
+            self.vectors.append(self.embedding_MiniLM_api(doc.page_content))
+        
+        return self.vectors, self.index, self.chunk_text
+
+    def get_index_pinecone(self):
+        if not self.pc.has_index(self.index_name):
+            self.pc.create_index(
+                name=self.index_name,
+                dimension=384,
+                metric='cosine',
+                spec={
+                    "serverless": {
+                        "cloud": "aws",
+                        "region": "us-east-1"
+                    }
+                }
+                
+            )
+        return self.pc.Index(self.index_name)
+
+    def get_vectorstore_pinecone(self, ids, embedding, chunk_text):
+        index = self.get_index_pinecone()
+        data = [ {
+                "id": str(ids),
+                "values": embedding[0],
+                "metadata": {"text": chunk_text}
+            }]
+        index.upsert(
+            namespace=self.namespace,
+            vectors=data
+        )
+        return index
 
 
-def documents(Path):
-    load_pdf = Pdf_loading(Path)
-    docs_split = text_chunk_split()
-    docs_spliting = docs_split.split_documents(Pdf_loading(Path))
-    return docs_spliting
+    def prompting(self, docs, user_input):
+        context = "\n\n".join([doc for doc in docs])
+        prompt = f"""
+            Dựa vào thông tin sau, hãy trả lời ngắn gọn và tự nhiên:
+            {context}
+            Câu hỏi: {user_input}
+            Trả lời: 
+        """
+        return prompt
 
-def indexing():
-    docs = documents(Path)
-    vectors, ids, chunk_text = get_vectorstore(docs)
-    index = get_vectorstore_pinecone(index_name, ids, vectors, chunk_text)
-    return index
-    
+
+    def documents(self, Path):
+        load_pdf = Pdf_loading(Path)
+        docs_split = text_chunk_split()
+        docs_spliting = docs_split.split_documents(Pdf_loading(Path))
+        return docs_spliting
+
+    def indexing(self):
+        docs = self.documents(self.Path_PDF)
+        vectors, ids, chunk_text = self.get_vectorstore(docs)
+        index = self.get_vectorstore_pinecone(ids, vectors, chunk_text)
+        return index
+        
    
 
     
